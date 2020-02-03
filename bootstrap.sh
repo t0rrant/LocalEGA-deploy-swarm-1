30b829c6325397818d25e6b69655cd1f856f697d
dtitov <dmytro.titov@gmail.com>
2020-02-03 13:21:28 +0100

    Add ELK logging

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,22 +4,22 @@ mkcert -install
 cp "$(mkcert -CAROOT)/rootCA.pem" rootCA.pem
 cp "$(mkcert -CAROOT)/rootCA-key.pem" rootCA-key.pem
 openssl pkcs12 -export -out rootCA.p12 -in rootCA.pem -inkey rootCA-key.pem -passout pass:"${ROOT_CERT_PASSWORD}"
-mkcert localhost db vault public-mq private-mq tsd proxy
-openssl pkcs12 -export -out localhost+6.p12 -in localhost+6.pem -inkey localhost+6-key.pem -passout pass:"${SERVER_CERT_PASSWORD}"
-mkcert -client localhost db vault public-mq private-mq tsd proxy
-openssl pkcs12 -export -out localhost+6-client.p12 -in localhost+6-client.pem -inkey localhost+6-client-key.pem -passout pass:"${CLIENT_CERT_PASSWORD}"
-openssl pkcs8 -topk8 -inform PEM -in localhost+6-client-key.pem -outform DER -nocrypt -out localhost+6-client-key.der
+mkcert localhost db vault public-mq private-mq tsd proxy kibana logstash elasticsearch
+openssl pkcs12 -export -out localhost+9.p12 -in localhost+9.pem -inkey localhost+9-key.pem -passout pass:"${SERVER_CERT_PASSWORD}"
+mkcert -client localhost db vault public-mq private-mq tsd proxy kibana logstash elasticsearch
+openssl pkcs12 -export -out localhost+9-client.p12 -in localhost+9-client.pem -inkey localhost+9-client-key.pem -passout pass:"${CLIENT_CERT_PASSWORD}"
+openssl pkcs8 -topk8 -inform PEM -in localhost+9-client-key.pem -outform DER -nocrypt -out localhost+9-client-key.der
 
 docker swarm init
 docker config create rootCA.pem rootCA.pem
 docker config create rootCA.p12 rootCA.p12
-docker config create server.pem localhost+6.pem
-docker config create server-key.pem localhost+6-key.pem
-docker config create server.p12 localhost+6.p12
-docker config create client.pem localhost+6-client.pem
-docker config create client-key.pem localhost+6-client-key.pem
-docker config create client-key.der localhost+6-client-key.der
-docker config create client.p12 localhost+6-client.p12
+docker config create server.pem localhost+9.pem
+docker config create server-key.pem localhost+9-key.pem
+docker config create server.p12 localhost+9.p12
+docker config create client.pem localhost+9-client.pem
+docker config create client-key.pem localhost+9-client-key.pem
+docker config create client-key.der localhost+9-client-key.der
+docker config create client.p12 localhost+9-client.p12
 docker config create jwt.pub.pem jwt.pub.pem
 
 echo "${KEY_PASSWORD}" > ega.sec.pass
@@ -36,5 +36,20 @@ perl -i -pe 's!DB_HOST!$ENV{"DB_HOST"}!g' conf.ini
 perl -i -pe 's!DB_LEGA_IN_PASSWORD!$ENV{"DB_LEGA_IN_PASSWORD"}!g' conf.ini
 perl -i -pe 's!MQ_CONNECTION!$ENV{"MQ_CONNECTION"}!g' conf.ini
 docker config create conf.ini conf.ini
+
+cp default.elasticsearch.yml elasticsearch.yml
+docker config create elasticsearch.yml elasticsearch.yml
+
+cp default.kibana.yml kibana.yml
+perl -i -pe 's!ELASTIC_PASSWORD!$ENV{"ELASTIC_PASSWORD"}!g' kibana.yml
+docker config create kibana.yml kibana.yml
+
+cp default.logstash.yml logstash.yml
+perl -i -pe 's!ELASTIC_PASSWORD!$ENV{"ELASTIC_PASSWORD"}!g' logstash.yml
+docker config create logstash.yml logstash.yml
+
+cp default.logstash.conf logstash.conf
+perl -i -pe 's!ELASTIC_PASSWORD!$ENV{"ELASTIC_PASSWORD"}!g' logstash.conf
+docker config create logstash.conf logstash.conf
 
 docker-compose config > docker-stack.yml
--- cleanup.sh
+++ cleanup.sh
@@ -11,4 +11,4 @@ docker config rm $(docker config list -q)
 
 # shellcheck disable=SC2035
 # shellcheck disable=SC2216
-rm conf.ini rootCA.pem rootCA-key.pem localhost+*.pem *.p12 localhost+*.der docker-stack.yml ega*.pem ega*.pass
+rm elasticsearch.yml kibana.yml logstash.yml logstash.conf conf.ini rootCA.pem rootCA-key.pem localhost+*.pem *.p12 localhost+*.der docker-stack.yml ega*.pem ega*.pass
